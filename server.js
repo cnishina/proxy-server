2df38e96f8f3bd3f8d99333411d9d5ef53e01b14
Craig Nishina <craig.nishina@gmail.com>
2016-08-22 17:58:03 -0700

    add proxy server

---
+++ server.js
@@ -0,0 +1,28 @@
+var httpProxy = require('http-proxy');
+var http = require('http');
+//var proxy = httpProxy.createProxyServer((req, res) => {
+//  console.log(req);
+//  return { target: 'https://selenium-release.storage.googleapis.com' };
+//});
+//
+//
+var seleniumProxy = httpProxy.createProxyServer({
+  target: 'https://selenium-release.storage.googleapis.com'
+});
+
+var chromeProxy = httpProxy.createProxyServer({
+  target: 'https://chromedriver.storage.googleapis.com'
+});
+
+var proxy = http.createServer(function(req, res) {
+  var header = req.headers['host'];
+  if (header.startsWith('selenium')) {
+    console.log(header);
+    seleniumProxy.web(req,res);
+  }
+  if (header.startsWith('chromedriver')) {
+    console.log(header);
+    chromeProxy.web(req,res);
+  }
+});
+proxy.listen(8000);
